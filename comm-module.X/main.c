24e82870411e06b41402374af5821dbd2aba0428
Maxime Guillemette <maxime_glmt@hotmail.ca>
2020-01-25 08:59:19 -0500

    added basic files to the project

---
+++ comm-module.X/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+int main(void) {
+    
+    
+    for(;;) {
+        
+    }
+    
+    return 0;
+}
---
+++ comm-module.X/main.h
@@ -0,0 +1,15 @@
+/*
+ * Main source file for the communication module of the rocket
+ * 
+ * Authors:
+ *      Simon Gaudy
+ *      Maxime Guillemette
+ *      Elliot Lafront
+ * 
+ * Brief:
+ *      TODO
+ */
+
+
+/* standard lib includes */
+#include "xc.h"
