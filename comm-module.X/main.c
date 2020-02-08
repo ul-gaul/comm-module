78ef433283de40411275720bf06d6355dbd7b092
Maxime Guillemette <maxime_glmt@hotmail.ca>
2020-02-08 17:08:01 -0500

    made UART transmitter at 19200 (20000 actual) baud rate

--- comm-module.X/main.c
+++ comm-module.X/main.c
@@ -1,12 +1,31 @@
 #include "main.h"
 
 
+const char message[] = "hello PIC32";
+
 int main(void) {
+    int i;
+
+	/* set PPS for UART1 to use pins D2 (Rx) and D3 (Tx) */
+//    PB2DIV = 0x00ff;
+	U1RXR = 0x00;
+	RPD3R = 0x01;
+
+	/* init UART1 bus with 115200 baudrate */
+	U1MODEbits.ON = 1;
+    U1STAbits.URXEN = 1;
+    U1STAbits.UTXEN = 1;
+    U1BRG = 13;
     
-    
-    for(;;) {
-        
-    }
-    
-    return 0;
+    i = 0;
+	for(;;) {
+		U1TXREG = message[i];
+        i++;
+        if (i >= 11) {
+            i = 0;
+        }
+        while (U1STAbits.TRMT == 0);
+	}
+	
+	return 0;
 }
