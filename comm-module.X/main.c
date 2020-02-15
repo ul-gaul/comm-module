66b21a1a6df85937af3cf65551a134686eff5fbd
Maxime Guillemette <maxime_glmt@hotmail.ca>
2020-02-15 15:37:45 -0500

    trying to get clock to work

--- comm-module.X/main.c
+++ comm-module.X/main.c
@@ -7,24 +7,32 @@ int main(void) {
     int i;
 
 	/* set PPS for UART1 to use pins D2 (Rx) and D3 (Tx) */
-//    PB2DIV = 0x00ff;
-	U1RXR = 0x00;
-	RPD3R = 0x01;
+//    PB2DIVbits.PBDIV = 0x0001;
+//    PB2DIVbits.ON = 1;
+//
+//	U1RXR = 0x00;
+//	RPD3R = 0x01;
 
 	/* init UART1 bus with 115200 baudrate */
-	U1MODEbits.ON = 1;
-    U1STAbits.URXEN = 1;
-    U1STAbits.UTXEN = 1;
-    U1BRG = 13;
+//	U1MODEbits.ON = 1;
+//    U1STAbits.URXEN = 1;
+//    U1STAbits.UTXEN = 1;
+//    U1BRG = 13;
     
     i = 0;
+    TRISDbits.TRISD3 = 0;
 	for(;;) {
-		U1TXREG = message[i];
-        i++;
-        if (i >= 11) {
-            i = 0;
-        }
-        while (U1STAbits.TRMT == 0);
+//		U1TXREG = message[i];
+//        i++;
+//        if (i >= 11) {
+//            i = 0;
+//        }
+//        while (U1STAbits.TRMT == 0);
+//        U1TXREG = 0x55;
+        
+        LATDbits.LATD3 = 0;
+        LATDbits.LATD3 = 1;
+        
 	}
 	
 	return 0;
