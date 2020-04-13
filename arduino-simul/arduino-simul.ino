9fd627ebc546fdead0190a1d34ba91bc9ca8991b
Maxime Guillemette <maxime_glmt@hotmail.ca>
2020-04-13 14:25:36 -0400

    creating an arduino simulator for the avionics and motor sensors

---
+++ arduino-simul/arduino-simul.ino
@@ -0,0 +1,25 @@
+/*
+ * Simulator for avionics and motor sensors
+ *
+ * Author:
+ * 		Maxime Guillemette
+ * 
+ * Brief:
+ * 		Simulates the avionics by sending avionics data packets
+ * 		(with slight randomness) and simulates the motor sensors
+ * 		using 5 analog outputs (all distinct values with small
+ * 		random offsets)
+ */
+
+
+void setup(void) {
+
+
+}
+
+
+void loop(void) {
+
+
+}
+
